574efe6595eb88866bbb98c15145abcbfe66c2a2
NicolasGMACK <nicolasgmack@gmail.com>
2025-07-15 10:54:30 -0300

    aula1

--- src/script/script.js
+++ src/script/script.js
@@ -1,3 +1,16 @@
 document.addEventListener("DOMContentLoaded", () => {
     console.log("Minha página carregou");
+
+    const inputMsg = document.querySelector('#inputMensagem');
+    console.log(inputMsg);
+
+    inputMsg.placeholder = "Digite a sua mensagem"
+
+    const buttons = document.querySelectorAll(".cursor--pointer");
+    console.log(buttons);
+
+    const buttonSend = document.querySelector(".cursor--pointer[src*='send']")
+    console.log(buttonSend);
+
+    buttonSend.classList.add("minha-classe-modulo-um");
 })
