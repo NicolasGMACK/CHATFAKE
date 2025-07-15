17a5076d6673ee2570553230c27134a3ab3d36e7
NicolasGMACK <nicolasgmack@gmail.com>
2025-07-15 11:24:17 -0300

    aula2

--- src/script/script.js
+++ src/script/script.js
@@ -4,7 +4,7 @@ document.addEventListener("DOMContentLoaded", () => {
     const inputMsg = document.querySelector('#inputMensagem');
     console.log(inputMsg);
 
-    inputMsg.placeholder = "Digite a sua mensagem"
+    inputMsg.placeholder = "Digite a sua mensagem!"
 
     const buttons = document.querySelectorAll(".cursor--pointer");
     console.log(buttons);
@@ -12,5 +12,30 @@ document.addEventListener("DOMContentLoaded", () => {
     const buttonSend = document.querySelector(".cursor--pointer[src*='send']")
     console.log(buttonSend);
 
-    buttonSend.classList.add("minha-classe-modulo-um");
+    //buttonSend.classList.add("minha-classe-modulo-um");
+
+    function enviarAlerta() {
+        const texto = inputMsg.value.trim();
+        if(texto === ""){
+            alert("Ainda não possui msg.");
+        } else{        
+        alert(texto);
+        }
+    }
+
+    buttonSend.addEventListener("click", () => {
+        enviarAlerta();
+    });
+
+ inputMsg.addEventListener("keypress", (event)=> {
+    if (event.key === "Enter") {
+            enviarAlerta();
+        }
+        })
+
+
+
+
+
+
 })
