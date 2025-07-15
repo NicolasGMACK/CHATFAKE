cf233accf132e7f00fe4b456465b8ab8431d2e59
NicolasGMACK <nicolasgmack@gmail.com>
2025-07-15 11:44:15 -0300

    aula3

--- src/script/script.js
+++ src/script/script.js
@@ -12,24 +12,42 @@ document.addEventListener("DOMContentLoaded", () => {
     const buttonSend = document.querySelector(".cursor--pointer[src*='send']")
     console.log(buttonSend);
 
+    const listaMensagens = document.querySelector(".div--messages");
+    console.log(listaMensagens);
     //buttonSend.classList.add("minha-classe-modulo-um");
 
-    function enviarAlerta() {
+    function enviarAlerta(tipo) {
         const texto = inputMsg.value.trim();
         if(texto === ""){
             alert("Ainda não possui msg.");
-        } else{        
-        alert(texto);
+        } else{ 
+        adicionarMensagem(tipo, texto);
         }
+    }
+    
+    function adicionarMensagem(tipoMensagem, texto){
+        const mensagemElement = document.createElement("div");
+        mensagemElement.classList.add("message");
+
+        if (tipoMensagem === "enviada") {
+            mensagemElement.classList.add('you');
+        } else {
+            mensagemElement.classList.add("other");
+        }
+
+        mensagemElement.innerText = texto;
+
+        listaMensagens.appendChild(mensagemElement);
     }
 
     buttonSend.addEventListener("click", () => {
-        enviarAlerta();
+        enviarAlerta("recebida");
+        
     });
 
  inputMsg.addEventListener("keypress", (event)=> {
     if (event.key === "Enter") {
-            enviarAlerta();
+            enviarAlerta("enviada");
         }
         })
 
