ea09bd00fa9353f96925bb97c4d26119a3e5a40a
NicolasGMACK <nicolasgmack@gmail.com>
2025-07-15 20:16:48 -0300

    aula 6

--- src/script/script.js
+++ src/script/script.js
@@ -1,3 +1,54 @@
+const listaDeContatos = [
+    {
+        id: 1,
+        nome: "Joaquim",
+        ultimaMensagem: "Olá, vamos programar?",
+        horario: "20:20",
+        avatar: "./src/assets/images/jessica--drew.png",
+        conversas: [
+            {mensagem: "a", tipo:"recebida", horario: "20:00"},
+            {mensagem: "a", tipo:"recebida", horario: "20:00"},
+            {mensagem: "a", tipo:"recebida", horario: "20:00"}
+        ]
+    },
+    {
+        id: 2,
+        nome: "Maria",
+        ultimaMensagem: "sei la kkkkk",
+        horario: "20:20",
+        avatar: "./src/assets/images/greg--james.png",
+        conversas: [
+            {mensagem: "a", tipo:"recebida", horario: "20:00"},
+            {mensagem: "a", tipo:"recebida", horario: "20:00"},
+            {mensagem: "a", tipo:"recebida", horario: "20:00"}
+        ]
+    },
+    {
+        id: 3,
+        nome: "Jose",
+        ultimaMensagem: "OGosto de vc",
+        horario: "20:20",
+        avatar: "./src/assets/images/emily--dorson.png",
+        conversas: [
+            {mensagem: "a", tipo:"recebida", horario: "20:00"},
+            {mensagem: "a", tipo:"recebida", horario: "20:00"},
+            {mensagem: "a", tipo:"recebida", horario: "20:00"}
+        ]
+    },
+    {
+        id: 4,
+        nome: "Nick",
+        ultimaMensagem: "faz o L",
+        horario: "20:20",
+        avatar: "./src/assets/images/david--moore.png",
+        conversas: [
+            {mensagem: "a", tipo:"recebida", horario: "20:00"},
+            {mensagem: "a", tipo:"recebida", horario: "20:00"},
+            {mensagem: "a", tipo:"recebida", horario: "20:00"}
+        ]
+    }
+]
+
 document.addEventListener("DOMContentLoaded", () => {
     console.log("Minha página carregou");
 
@@ -78,8 +129,49 @@ document.addEventListener("DOMContentLoaded", () => {
         })
 
 
+function carregarContatos() {
+    const divContatosElement = document.querySelector(".div--contacts");
+
+    
+    listaDeContatos.forEach((contato) => {
+        console.log(contato);
+        //inner text e o inner html
+
+        const divParentElement = document.createElement("div");
+        divParentElement.classList.add("flex", "area--contact", "fade-in");
+    
+        divParentElement.innerHTML = 
+        `       
+            <div class="flex justify--content--center align--items--center flex--1">
+                <img class="avatar--left--bar" src="${contato.avatar}" />
+            </div>
+
+            <div class="flex flex--direction--column justify--content--center flex--3">
+                <div class="flex align--items--center infos--contact">
+                    <div class="font--family font--weight--bold">${contato.nome}</div>
+                    <img src="./src/assets/icons/verified.svg" />
+                </div>
+
+                <div class="last--message">${contato.ultimaMensagem}</div>
+                
+            </div>
+
+            <div class="flex flex--direction--column justify--content--center align--items--end flex--1 div--last--messages--info">
+                <div class="hour--last--message">${contato.horario}</div>
+                <div class="flex justify--content--center align--items--center quantity--not--viewed--messages background--green">1</div>
+            </div>
+        `;        
+        
+        divContatosElement.appendChild(divParentElement);
+    });
+
+   
+}
+
+setTimeout(() => {
+ carregarContatos();
+}, 2500);
 
 
 
-
-})
+});
